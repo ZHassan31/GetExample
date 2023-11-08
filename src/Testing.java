7044e029f68e9282f8dc25c80ef0e25a5927f974
ZHassan31 <zubayr141@hotmail.com>
2023-11-08 13:21:04 +0000

    Update Testing.java

--- src/Testing.java
+++ src/Testing.java
@@ -4,7 +4,11 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HEllo World");
-
+		int a,b,c;
+		a=5;
+		b=4;
+		c=a*b;
+		System.out.println(c);
 	}
 
 }
